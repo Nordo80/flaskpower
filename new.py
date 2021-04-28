08afe82e7e95c9100461212c958cacf3da29479d
Nordo80 <aleksandr.borovkov97@gmail.com>
2021-04-28 16:14:53 +0300

    lflwe

--- new.py
+++ new.py
@@ -1,3 +1,2 @@
 import os
-os.system("mysqldump -u newuser -ppassword flask > " + os.path.dirname(os.path.abspath(__file__)) + "/flask.sql")
 os.system("mysql -u b12f26d01aa967 -pb85dc090 heroku_b40d4a2594424bc -h eu-cdbr-west-01.cleardb.com < " + os.path.dirname(os.path.abspath(__file__)) + "/flask.sql")
