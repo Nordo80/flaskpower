9907e55a6ca454d4e708a7f23190ab9110039e3d
Nordo80 <aleksandr.borovkov97@gmail.com>
2021-04-28 15:40:13 +0300

    lslsls

--- new.py
+++ new.py
@@ -1,3 +1,4 @@
 import os
-os.system("mysqldump -u newuser -ppassword flask > /home/aleksandr/flask.sql")
-os.system("mysql -u b12f26d01aa967 -pb85dc090 heroku_b40d4a2594424bc -h eu-cdbr-west-01.cleardb.com < /home/aleksandr/flask.sql")
+"""os.system("mysqldump -u newuser -ppassword flask > /home/aleksandr/flask.sql")
+os.system("mysql -u b12f26d01aa967 -pb85dc090 heroku_b40d4a2594424bc -h eu-cdbr-west-01.cleardb.com < /home/aleksandr/flask.sql")"""
+os.system("ls -l")
