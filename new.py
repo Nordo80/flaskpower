15472eaaca5b66f83db3c5b2622c64bc2b4c69ea
Nordo80 <aleksandr.borovkov97@gmail.com>
2021-04-28 15:55:23 +0300

    lslsls

--- new.py
+++ new.py
@@ -1,4 +1,3 @@
 import os
-os.system("mysqldump -u newuser -ppassword flask > " os.path.dirname(os.path.abspath(__file__)) + "/flask.sql")
-os.system("mysql -u b12f26d01aa967 -pb85dc090 heroku_b40d4a2594424bc -h eu-cdbr-west-01.cleardb.com < /home/aleksandr/flask.sql")
-print(os.path.dirname(os.path.abspath(__file__)) + "/lala")
+os.system("mysqldump -u newuser -ppassword flask > " + os.path.dirname(os.path.abspath(__file__)) + "/flask.sql")
+os.system("mysql -u b12f26d01aa967 -pb85dc090 heroku_b40d4a2594424bc -h eu-cdbr-west-01.cleardb.com < " + os.path.dirname(os.path.abspath(__file__)) + "/flask.sql")
